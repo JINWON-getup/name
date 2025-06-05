4dcf06789ed8e945f6231126a1558ed796a6ab56
jin <wlsdnjs3225@naver.com>
2025-06-05 11:38:46 +0900

    0605 마지막 내용

--- src/store.jsx
+++ src/store.jsx
@@ -1,25 +1,26 @@
-import { create } from 'zustand';
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
-// const MOCK_DATA = [
-//   {
-//     id: 1,
-//     title: '작업 목록 1',
-//     desc: '해야 할 작업 목록입니다. 우선순위를 정하고 진행하세요.',
-//     type: 'todo',
-//     created_at: '2023-10-01',
-//   },
-// ]
-
-export const useBoardStore = create((set) => ({
-    boards: [{}],
-    addBoard: (board) => set((state) => ({
-        boards: [...state.boards, board]
-    })), 
-    removeBoard: (id) => set((state) => ({
-        boards: state.boards.filter((item) => item.id !== id),
-    })),
-    editBoard: (id, board) =>
+export const useBoardStore = create(
+  persist(
+    (set) => ({
+      boards: [],
+      addBoard: (board) =>
         set((state) => ({
-            boards: state.boards.map((item) => (item.id == id ? board : item)),
+          boards: [...state.boards, board],
         })),
-}));
+      removeBoard: (id) =>
+        set((state) => ({
+          boards: state.boards.filter((item) => item.id !== id),
+        })),
+      editBoard: (id, board) =>
+        set((state) => ({
+          boards: state.boards.map((item) => (item.id === id ? board : item)),
+        })),
+    }),
+    {
+      name: 'boards',
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
